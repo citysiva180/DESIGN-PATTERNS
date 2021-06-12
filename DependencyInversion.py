448897a1210f1981121d6badb05134543860bab3
Sivarajan <citysiva180@gmail.com>
2021-06-12 06:26:08 +0530

    removed unwanted comment

--- DependencyInversion.py
+++ DependencyInversion.py
@@ -7,7 +7,7 @@
 # High-level modules should not depend on low-level modules. Both should depend on abstractions (e.g. interfaces).
 # Abstractions should not depend on details. Details (concrete implementations) should depend on abstractions
 
-# EXAMPLE PROGREAM
+# EXAMPLE PROGRAM
 
 
 from abc import ABC, abstractmethod
